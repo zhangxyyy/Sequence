edf54d4db7372abcb678538fe22501f3907361fc
zhangxyyy <1477586942@qq.com>
2018-07-07 10:57:29 +0800

    update

--- Sequence.cpp
+++ Sequence.cpp
@@ -19,36 +19,28 @@ while(file.get(s))
 line++;}
 q=m-line;}
 
-Sequence::~Sequence()
-{
-}
+
 int Sequence::length()
 {
 cout<<q<<endl;
 return 0;
 }
 int Sequence::numberOf(char base)
-{
+{ifstream file(z);
+int k=0;
+char s;
+while (file.get(s))
+{if (s==base)
+k++;
+else
+continue;}
+cout<<base<<k<<endl;
 return 0;
+
+}
 string Sequence::longestConsecutive()
-{char b="0";
- char base;
-int max=0;
-int add=1;
-int L=STr.length();
-for(int i=0;i<L-1;i++)
- {if(STr[i+1]==STr[i])
-     {add++;
-      }
-else {
-        if (add>max)
-         {max=add;
-          base=STr[i];}
-add=1;
-}
-}
-string longestcon(max,base);
-return longestcon;
+{return 0;
+
 }
 string Sequence::longestRepeated()
 {return 0;
