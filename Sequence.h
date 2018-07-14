12323127b20e53361ef1ceda254e03e9c573d334
zhangxyyy <1477586942@qq.com>
2018-07-14 10:17:35 +0800

    update

--- Sequence.h
+++ Sequence.h
@@ -1,4 +1,4 @@
-#ifndef SEQUENCE_H
+/*#ifndef SEQUENCE_H
 #define SEQUENCE_H
 #include<fstream>
 #include<string>
@@ -18,4 +18,57 @@ string longestConsecutive();
 string longestRepeated();
 };
 #endif
+*/
+#ifndef SEQUENCE_H
+
+#define SEQUENCE_H
+
+
+#include<iostream>
+
+#include<stdio.h>
+
+#include<string>
+
+#include<vector>
+
+#include<algorithm>
+
+#include<cstring>
+
+using namespace std;
+
+
+class Sequence
+
+{
+
+public:
+
+	Sequence();
+
+		~Sequence();
+
+	Sequence(const string& filename);
+
+	int length();
+
+	int numberOf(char base);
+
+	string longestConsecutive();
+
+	string longestRepeated();
+
+
+private:
+
+	int count(const char*c1, const char*c2);
+
+	string value;
+
+	int numberA, numberC, numberG, numberT;
+
+};
+
+#endif 
 
